0346f3f785b969424eec3821de23f8842891356a
iliza-alufp <iliza@alu.fpsanantonio.com>
2026-05-06 00:12:06 +0200

    Add files via upload

---
+++ MarioCartas.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE MarioCartas;
+
+USE MarioCartas;
+
+-- Tabla de los personajes
+
+CREATE TABLE IF NOT EXISTS personajes (
+
+id_personaje INT AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(20),
+id_clase INT
+);
+
+-- Tabla de las clases
+
+CREATE TABLE IF NOT EXISTS personajes (
+
+id_clase INT AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(20),
+FOREIGN KEY (id_clase) REFERENCES personajes (id_clase) ON DELETE CASCADE
+);
+
+-- Tabla de las habilidades
+
+CREATE TABLE IF NOT EXISTS habilidades (
+
+id_habilidad INT AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(20),
+descripcion VARCHAR(50)
+);
+
+-- Tabla de los objetos
+
+CREATE TABLE IF NOT EXISTS objetos (
+
+id_habilidad INT AUTO_INCREMENT PRIMARY KEY,
+objeto VARCHAR(20),
+efecto VARCHAR(50)
+);
+
