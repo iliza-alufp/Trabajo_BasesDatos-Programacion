c7c67c435ce9ca03a2380c8a1f29652b618265d7
srodes <srodes@alu.fpsanantonio.com>
2026-05-06 11:47:16 +0200

    Add new columns to personajes and objetos tables

--- MarioCartas.sql
+++ MarioCartas.sql
@@ -8,12 +8,15 @@ CREATE TABLE IF NOT EXISTS personajes (
 
 id_personaje INT AUTO_INCREMENT PRIMARY KEY,
 nombre VARCHAR(20),
+vida INT,
+ataque INT,
+coste INT,
 id_clase INT
 );
 
 -- Tabla de las clases
 
-CREATE TABLE IF NOT EXISTS personajes (
+CREATE TABLE IF NOT EXISTS clases (
 
 id_clase INT AUTO_INCREMENT PRIMARY KEY,
 nombre VARCHAR(20),
@@ -35,6 +38,8 @@ CREATE TABLE IF NOT EXISTS objetos (
 
 id_habilidad INT AUTO_INCREMENT PRIMARY KEY,
 objeto VARCHAR(20),
+coste INT,
+ataque INT,
 efecto VARCHAR(50)
 );
 
